f052c5f51d74a2455c32dcbfe5e4821e32347ab5
tcw57 <49931324+tcw57@users.noreply.github.com>
2019-06-18 10:09:56 -0400

    Added initial files

---
+++ sorter.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <sstream>
+#include "hexbin.h"
+
+using namespace std;
+
+int main(){
+	const int nphibins = 27; //number of phi bins
+	const int nphisectors = 9; //number of phi sectors
+	const int tpe = 224; //track per event
+	ifstream in_tracks[2][nphisectors];
+	int ntrks[nphibins];
+	for (int i = 0; i < nphisectors; ++i) {
+		string fname1, fname2; //"1" is for positive eta files, "2" is for negative eta files.
+		ifstream file1, file2;
+		string s;
+		stringstream out;
+		out << i;
+		s = out.str();
+		fname1 = "phi" + s + "_n.dat";
+		fname2 = "phi" + s + "_p.dat";
+		in_tracks[0][i].open(fname1);
+		in_tracks[1][i].open(fname2);
+	}
+	for (int j = 0; j < nphibins; ++j){
+		ntrks[j] = 0;
+	}
+//This part is for debugging purposes/////////
+	//string line;
+	//while (getline(in_tracks[1][7],line)){
+	//	cout << line << endl;
+	//}
+//////////////////////////////////////////////
+
+//initialize new input files
+	ofstream in_tracks_final[nphibins];
+	for (int i = 0; i < nphibins; ++i){
+		string fname;
+		string s;
+		stringstream out;
+		out << i;
+		s = out.str();
+		fname = "phi" + s + ".dat";
+		in_tracks_final[i].open(fname);
+	}
+
+	for (int i = 0; i < nphisectors; ++i){
+		string line1, line2;
+		for (int j = 0; j < nphibins; ++j){
+            //fill each phi file, from 0 to 26, one event at a time
+			while (getline(in_tracks[0][i],line1) && getline(in_tracks[1][i],line2) && ntrks[j] < 339){
+				if (line1 != "0x000000000000000000000000" && line2 != "0x000000000000000000000000"){
+					in_tracks_final[j] << line1 << endl;
+					in_tracks_final[j] << line2 << endl;
+					ntrks[j] += 2;
+				}
+				else if (line1 != "0x000000000000000000000000" && line2 == "0x000000000000000000000000"){
+					in_tracks_final[j] << line1 << endl;
+					ntrks[j] += 1;
+				}
+				else if (line1 == "0x000000000000000000000000" && line2 != "0x000000000000000000000000"){
+					in_tracks_final[j] << line2 << endl;
+					ntrks[j] += 1;
+				}
+				else{
+					for (int k = 0; k < (tpe - ntrks[j]); ++k){
+						in_tracks_final[j] << "0x000000000000000000000000" << endl;
+					}
+                    			continue;
+				}
+			}
+			in_tracks_final[j].close();
+			in_tracks[0][i].close();
+			in_tracks[1][i].close();
+		}
+
+	}
+
+	return 0;
+}
