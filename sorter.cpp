69925513ef96936e339f335ea906df81285c3cc1
tcw57 <skylerskyler732@gmail.com>
2019-06-18 22:37:12 -0400

    lots of changes

--- sorter.cpp
+++ sorter.cpp
@@ -8,6 +8,7 @@
 
 using namespace std;
 const int nphibits = 12;
+const float phi_bin_width = (2*M_PI)/27;
 float int_to_float(int int_num){
 	float floatval = (int_num/(pow(2,nphibits-1)-1))*M_PI;
 	return floatval;
@@ -34,13 +35,6 @@ int main(){
 	for (int j = 0; j < nphibins; ++j){
 		ntrks[j] = 0;
 	}
-//This part is for debugging purposes/////////
-	//string line;
-	//while (getline(in_tracks[1][7],line)){
-	//	cout << line << endl;
-	//}
-//////////////////////////////////////////////
-
 //initialize new input files
 	ofstream in_tracks_final[nphibins];
 	for (int i = 0; i < nphibins; ++i){
@@ -55,32 +49,109 @@ int main(){
 
 	for (int i = 0; i < nphisectors; ++i){
 		string line1, line2;
-		for (int j = 0; j < nphibins; ++j){
-            //fill each phi file, from 0 to 26, one event at a time
-			while (getline(in_tracks[0][i],line1) && getline(in_tracks[1][i],line2) && ntrks[j] < 339){
-				if (line1 != "0x000000000000000000000000" && line2 != "0x000000000000000000000000"){
-					in_tracks_final[j] << line1 << endl;
-					in_tracks_final[j] << line2 << endl;
-					ntrks[j] += 2;
-				}
-				else if (line1 != "0x000000000000000000000000" && line2 == "0x000000000000000000000000" && ntrks[j] < 339){
-					in_tracks_final[j] << line1 << endl;
-					ntrks[j] += 1;
-				}
-				else if (line1 == "0x000000000000000000000000" && line2 != "0x000000000000000000000000" && ntrks[j] < 339){
-					in_tracks_final[j] << line2 << endl;
-					ntrks[j] += 1;
-				}
-				else{
-					for (int k = 0; k < (tpe - ntrks[j]); ++k){
-						in_tracks_final[j] << "0x000000000000000000000000" << endl;
+            	//fill each phi file, from 0 to 26, one event at a time
+		while (getline(in_tracks[0][i],line1) && getline(in_tracks[1][i],line2)){
+			string phi_bin1 = hex_to_bin(line1.substr(2,24),96).substr(15,12);
+			string phi_bin2 = hex_to_bin(line2.substr(2,24),96).substr(15,12); 
+			int int1 = bin_to_int(phi_bin1);
+			int int2 = bin_to_int(phi_bin2);
+			float phi_float1 = int_to_float(int1);
+			float phi_float2 = int_to_float(int2); 
+			if (line1 != "0x000000000000000000000000" && line2 != "0x000000000000000000000000"){
+				if (phi_float1 < -M_PI/27){
+					in_tracks_final[3*i] << line1 << endl;
+					ntrks[3*i] += 1;
+					if (phi_float2 < -M_PI/27){
+					in_tracks_final[3*i] << line2 << endl;	
+					ntrks[3*i] += 1;
 					}
-                    			continue;
+					else if (phi_float2 >= -phi_bin_width/2 && phi_float2 < phi_bin_width/2){	
+						in_tracks_final[3*i+1] << line2 << endl;
+						ntrks[3*i+1] += 1;
+					}
+					else if (phi_float2 >= phi_bin_width/2){
+						in_tracks_final[3*i+2] << line2 << endl;
+						ntrks[3*i+2] += 1;
+					}
+				}
+				else if (phi_float1 >= -phi_bin_width/2 && phi_float1 < phi_bin_width/2){
+					in_tracks_final[3*i+1] << line1 << endl;
+					ntrks[3*i+1] += 1;
+					if (phi_float2 < -M_PI/27){
+						in_tracks_final[3*i] << line2 << endl;	
+						ntrks[3*i] += 1;
+					}
+					else if (phi_float2 >= -phi_bin_width/2 && phi_float2 < phi_bin_width/2){	
+						in_tracks_final[3*i+1] << line2 << endl;
+						ntrks[3*i+1] += 1;
+					}
+					else if (phi_float2 >= phi_bin_width/2){
+						in_tracks_final[3*i+2] << line2 << endl;
+						ntrks[3*i+2] += 1;
+					}
+				}
+				else if (phi_float1 >= phi_bin_width/2){
+					in_tracks_final[3*i+2] << line1 << endl;
+					ntrks[3*i+2] += 1;
+					if (phi_float2 < -M_PI/27){
+					in_tracks_final[3*i] << line2 << endl;	
+					ntrks[3*i] += 1;
+					}
+					else if (phi_float2 >= -phi_bin_width/2 && phi_float2 < phi_bin_width/2){	
+						in_tracks_final[3*i+1] << line2 << endl;
+						ntrks[3*i+1] += 1;
+					}
+					else if (phi_float2 >= phi_bin_width/2){
+						in_tracks_final[3*i+2] << line2 << endl;
+						ntrks[3*i+2] += 1;
+					}
+				}
+				
+			}
+			else if (line1 != "0x000000000000000000000000" && line2 == "0x000000000000000000000000"){
+				if (phi_float1 < -M_PI/27){
+					in_tracks_final[3*i] << line1 << endl;
+					ntrks[3*i] += 1;
+				}
+				else if (phi_float1 >= -phi_bin_width/2 && phi_float1 < phi_bin_width/2){
+					in_tracks_final[3*i+1] << line1 << endl;
+					ntrks[3*i+1] += 1;
+				}
+				else if (phi_float1 >= phi_bin_width/2){
+					in_tracks_final[3*i+2] << line1 << endl;
+					ntrks[3*i+2] += 1;
 				}
 			}
-			in_tracks_final[j].close();
-			in_tracks[0][i].close();
-			in_tracks[1][i].close();
+			else if (line1 == "0x000000000000000000000000" && line2 != "0x000000000000000000000000"){
+				if (phi_float2 < -M_PI/27){
+					in_tracks_final[3*i] << line2 << endl;	
+					ntrks[3*i] += 1;
+				}
+				else if (phi_float2 >= -phi_bin_width/2 && phi_float2 < phi_bin_width/2){	
+					in_tracks_final[3*i+1] << line2 << endl;
+					ntrks[3*i+1] += 1;
+				}
+				else if (phi_float2 >= phi_bin_width/2){
+					in_tracks_final[3*i+2] << line2 << endl;
+					continue;
+				}
+			}
+			else{
+				for (int k = 0; k < (3 + tpe - ntrks[3*i]); ++k){
+					in_tracks_final[3*i] << "0x000000000000000000000000" << endl;
+				}
+				for (int k = 0; k < (3 + tpe - ntrks[3*i+1]); ++k){
+					in_tracks_final[3*i+1] << "0x000000000000000000000000" << endl;
+				}
+				for (int k = 0; k < (3 + tpe - ntrks[3*i+2]); ++k){
+					in_tracks_final[3*i+2] << "0x000000000000000000000000" << endl;
+				}
+			}
+		in_tracks_final[3*i].close();
+		in_tracks_final[3*i+1].close();
+		in_tracks_final[3*i+2].close();
+		in_tracks[0][i].close();
+		in_tracks[1][i].close();
 		}
 
 	}
